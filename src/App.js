f4ba134a50c782f174591e2828830cbc46a87893
ParthParolekar <parolekarparth@gmail.com>
2022-05-08 12:38:00 +0530

    fix: test user deleted

--- src/App.js
+++ src/App.js
@@ -8,22 +8,22 @@ import Login from "./pages/Login/Login";
 import Signup from "./pages/Signup/Signup";
 
 export default function App() {
-  useEffect(() => {
-    localStorage.setItem(
-      "users",
-      JSON.stringify([
-        {
-          _id: uuid(),
-          firstName: "test",
-          lastName: "user",
-          email: "test@gmail.com",
-          password: "test123",
-          projectsApplied: [],
-          projectsCreated: []
-        }
-      ])
-    );
-  }, []);
+  // useEffect(() => {
+  //   localStorage.setItem(
+  //     "users",
+  //     JSON.stringify([
+  //       {
+  //         _id: uuid(),
+  //         firstName: "test",
+  //         lastName: "user",
+  //         email: "test@gmail.com",
+  //         password: "test123",
+  //         projectsApplied: [],
+  //         projectsCreated: []
+  //       }
+  //     ])
+  //   );
+  // }, []);
   return (
     <div className="App">
       {" "}
